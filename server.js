0c1df75eff931bef8769734532096a0739e1b6e7
Quan <qhoang2373@gmail.com>
2024-09-21 19:26:58 -0700

    add dotenv, touch env, and touch gitignore, within gitignore add .env and node_modules, within server.js add require('dotenv') as well as mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 //===============inputs================//
-
+require('dotenv').config()
 const express = require('express');
 const app = express ();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDb')
+})
+
 
 //===============Mongoose================//
 
