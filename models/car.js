663b42e6830912a5d249a06286739d977afe2a37
Quan <qhoang2373@gmail.com>
2024-09-21 19:39:02 -0700

    create car schema and mongoose models

---
+++ models/car.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    name: String,
+    model: String,
+    make: String,
+    isFast: Boolean,
+})
+
+const Car = mongoose.model('Car', carSchema)
+module.exports = Car
